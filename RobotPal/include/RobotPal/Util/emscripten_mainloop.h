84445a935eb171f7a499d7d91b5a457398567206
Junwoo Seo <junwoo.seo.1998@gmail.com>
2025-11-15 14:42:50 +0900

    Refactor CMake configuration and add Emscripten support
    - Removed ImPlot dependency from CMakeLists.txt and related code.
    - Added a new header file for Emscripten main loop management.
    - Implemented main application logic in main.cpp with ImGui integration.
    - Configured GLFW for OpenGL ES context and adjusted rendering loop for Emscripten compatibility.

---
+++ RobotPal/include/RobotPal/Util/emscripten_mainloop.h
@@ -0,0 +1,11 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <functional>
+static std::function<void()>            MainLoopForEmscriptenP;
+static void MainLoopForEmscripten()     { MainLoopForEmscriptenP(); }
+#define EMSCRIPTEN_MAINLOOP_BEGIN       MainLoopForEmscriptenP = [&]() { do
+#define EMSCRIPTEN_MAINLOOP_END         while (0); }; emscripten_set_main_loop(MainLoopForEmscripten, 0, true)
+#else
+#define EMSCRIPTEN_MAINLOOP_BEGIN
+#define EMSCRIPTEN_MAINLOOP_END
+#endif
--- RobotPal/src/main.cpp
+++ RobotPal/src/main.cpp
@@ -7,19 +7,7 @@
 #include<vector>
 #include<limits>
 
-
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#include <functional>
-static std::function<void()>            MainLoopForEmscriptenP;
-static void MainLoopForEmscripten()     { MainLoopForEmscriptenP(); }
-#define EMSCRIPTEN_MAINLOOP_BEGIN       MainLoopForEmscriptenP = [&]() { do
-#define EMSCRIPTEN_MAINLOOP_END         while (0); }; emscripten_set_main_loop(MainLoopForEmscripten, 0, true)
-#else
-#define EMSCRIPTEN_MAINLOOP_BEGIN
-#define EMSCRIPTEN_MAINLOOP_END
-#endif
-
+#include <RobotPal/Util/emscripten_mainloop.h>
 
 float value;
 std::vector<float> my_data = { 1.0f, 2.5f, 1.8f, 3.2f, 2.0f }; // Example data
