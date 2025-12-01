66f4c3e40cf01fdfc8c7e112f8df332c54e48fe1
Junwoo Seo <junwoo.seo.1998@gmail.com>
2025-12-02 01:05:24 +0900

    added nlohmann_json support

--- RobotPal/src/Tinygltf_impl.cpp
+++ RobotPal/src/Tinygltf_impl.cpp
@@ -1,8 +1,11 @@
 #define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_JSON 
 #define TINYGLTF_NO_INCLUDE_STB_IMAGE 
 #define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE 
 // #define TINYGLTF_NOEXCEPTION
 // #define JSON_NOEXCEPTION
+
+#include <json.hpp>
 
 #include <stb_image.h>
 #include <stb_image_write.h>
