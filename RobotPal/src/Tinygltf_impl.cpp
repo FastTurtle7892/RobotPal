e94f12a289442345a58402da7aefce6856627430
Junwoo Seo <junwoo.seo.1998@gmail.com>
2025-12-02 00:58:29 +0900

    made tinygltf use our linked stb library

--- RobotPal/src/Tinygltf_impl.cpp
+++ RobotPal/src/Tinygltf_impl.cpp
@@ -1,5 +1,10 @@
 #define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE 
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE 
 // #define TINYGLTF_NOEXCEPTION
 // #define JSON_NOEXCEPTION
 
-#include "tiny_gltf.h"
+#include <stb_image.h>
+#include <stb_image_write.h>
+
+#include <tiny_gltf.h>
